d9dca16ae0f9fa9261672ad6ca31c9e19e564dc9
R15L <randy@r16.ai>
2025-06-03 09:55:40 -0400

    Direx link in footer.

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -40,53 +40,69 @@
 				</div>
 			</div>
 			<!-- Right Side -->
-			<div class="flex flex-row gap-24">
-				<div class="flex flex-col gap-2">
-					<h3 class="text-lg leading-none font-medium text-slate-950 dark:text-slate-50">Links</h3>
-					<ul class="flex flex-col gap-1">
-						<li>
-							<a
-								href="/"
-								class={`text-sm leading-normal font-light transition-all duration-300
+			<div class="flex flex-col gap-8">
+				<div class="flex flex-row gap-24">
+					<div class="flex flex-col gap-2">
+						<h3 class="text-lg leading-none font-medium text-slate-950 dark:text-slate-50">
+							Links
+						</h3>
+						<ul class="flex flex-col gap-1">
+							<li>
+								<a
+									href="/"
+									class={`text-sm leading-normal font-light transition-all duration-300
 								${$page.url.pathname === '/' ? 'underline-dotted text-slate-950 underline dark:text-slate-50' : 'text-slate-600 hover:text-slate-950 dark:text-slate-300 hover:dark:text-slate-50'}`}
-							>
-								Directory
-							</a>
-						</li>
-						<li>
-							<a
-								href="/faq"
-								class={`text-sm leading-normal font-light transition-all duration-300
+								>
+									Directory
+								</a>
+							</li>
+							<li>
+								<a
+									href="/faq"
+									class={`text-sm leading-normal font-light transition-all duration-300
 								${$page.url.pathname === '/faq' ? 'underline-dotted text-slate-950 underline dark:text-slate-50' : 'text-slate-600 hover:text-slate-950 dark:text-slate-300 hover:dark:text-slate-50'}`}
-							>
-								FAQ
-							</a>
-						</li>
-					</ul>
-				</div>
-				<div class="flex flex-col gap-2">
-					<h3 class="text-lg leading-none font-medium text-slate-950 dark:text-slate-50">Sites</h3>
-					<ul class="flex flex-col gap-1">
-						<li>
-							<a
-								href="/terms"
-								class={`text-sm leading-normal font-light transition-all duration-300
+								>
+									FAQ
+								</a>
+							</li>
+						</ul>
+					</div>
+					<div class="flex flex-col gap-2">
+						<h3 class="text-lg leading-none font-medium text-slate-950 dark:text-slate-50">
+							Sites
+						</h3>
+						<ul class="flex flex-col gap-1">
+							<li>
+								<a
+									href="/terms"
+									class={`text-sm leading-normal font-light transition-all duration-300
 								${$page.url.pathname === '/terms' ? 'underline-dotted text-slate-950 underline dark:text-slate-50' : 'text-slate-600 hover:text-slate-950 dark:text-slate-300 hover:dark:text-slate-50'}`}
-							>
-								Terms of Use
-							</a>
-						</li>
-						<li>
-							<a
-								href="/privacy"
-								class={`text-sm leading-normal font-light transition-all duration-300
+								>
+									Terms of Use
+								</a>
+							</li>
+							<li>
+								<a
+									href="/privacy"
+									class={`text-sm leading-normal font-light transition-all duration-300
 								${$page.url.pathname === '/privacy' ? 'underline-dotted text-slate-950 underline dark:text-slate-50' : 'text-slate-600 hover:text-slate-950 dark:text-slate-300 hover:dark:text-slate-50'}`}
-							>
-								Privacy Policy
-							</a>
-						</li>
-					</ul>
+								>
+									Privacy Policy
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
+
+				<p class="text-sm leading-normal font-light text-slate-600 dark:text-slate-300">
+					🇺🇸 Made in Maryland, USA &amp; <br />⚡ Powered by
+					<a
+						href="https://axcai.gumroad.com/l/axc-0002"
+						target="_blank"
+						class="underline-dotted text-slate-950 underline dark:text-slate-50"
+						>Direx — Web Directory.</a
+					>
+				</p>
 			</div>
 		</div>
 	</div>
